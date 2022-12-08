387a76d3ab96e3cf5347afa1ccfdf0f2253fbb66
bog500 <limbo_9@hotmail.com>
2022-12-08 12:56:20 -0500

    Day 1

---
+++ Advent2022_Day1/Program.cs
@@ -0,0 +1,26 @@
+﻿int currentCal = 0;
+
+var lines = File.ReadAllLines("clue.txt");
+
+List<int> cal = new();
+
+foreach(string line in lines)
+{
+    if(line == "")
+    {
+        cal.Add(currentCal);
+        currentCal = 0;
+    }
+    else
+    {
+        currentCal += int.Parse(line);
+    }
+}
+
+Console.WriteLine("Part1:" + cal.Max());
+
+int top3 = cal.OrderDescending().Take(3).Sum();
+
+Console.WriteLine("Part2:" + top3);
+
+Console.ReadKey();
